f86a9d9f3a12b5edfd47363314bc99e62f415586
Tom-Shimoyama <125033423+Tom-Shimoyama@users.noreply.github.com>
2023-03-12 21:42:22 +0900

    第５回目課題用

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
@@ -8,36 +7,35 @@ public class StreamMain {
 
         //乱数生成
         Random rnd = new Random();
-        int num1 = rnd.nextInt(300);
-        int num2 = rnd.nextInt(400);
-        int num3 = rnd.nextInt(1500);
-        int num4 = rnd.nextInt(10000);
+        int num1 = rnd.nextInt(200);
+        int num2 = rnd.nextInt(200);
+        int num3 = rnd.nextInt(200);
+        int num4 = rnd.nextInt(200);
+        int num5 = rnd.nextInt(200);
+
 
         //List
-        List<Integer> numList = new ArrayList<>();
-
-        //要素
-        numList.add(num1);
-        numList.add(num2);
-        numList.add(num3);
-        numList.add(num4);
-
+        List<Integer> numList = List.of(num1, num2, num3, num4, num5);
         System.out.println(numList);
 
         //最大値の出力
-        List<Integer> maxresult = numList.stream().max(Comparator.naturalOrder()).stream().toList();
-        System.out.println("リストの最大値");
-        System.out.println(maxresult);
+        Integer maxresult = numList.stream().max(Comparator.naturalOrder()).orElse(0);
+        System.out.println("リストの最大値は" + maxresult + "です。" );
 
         //最小値の出力
-        List<Integer> minresult = numList.stream().min(Comparator.naturalOrder()).stream().toList();
-        System.out.println("リストの最小値");
-        System.out.println(minresult);
+        Integer minresult = numList.stream().min(Comparator.naturalOrder()).orElse(0);
+        System.out.println("リストの最小値は" + minresult + "です。");
 
         //平均値の出力
         double averageresult = numList.stream().mapToDouble(a -> a).average().orElse(0);
-        System.out.println("リストの平均値");
-        System.out.println(averageresult);
+        System.out.println("リストの平均値は" + averageresult + "です。");
+
+        List<Integer> evennamberresult = numList.stream().filter(i ->i%2==0).sorted().toList();
+        long countresult = numList.stream().filter(i ->i%2==0).count();
+        System.out.println("リストの偶数を表示します。");
+        System.out.println(evennamberresult);
+        System.out.println("偶数の値は" + countresult + "つです。");
+
 
     }
 }
