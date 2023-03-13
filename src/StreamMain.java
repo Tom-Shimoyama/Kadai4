72ddd9a8bb38020cca416885f555c83135be7cd8
Tom-Shimoyama <125033423+Tom-Shimoyama@users.noreply.github.com>
2023-03-13 20:54:04 +0900

    変数名をキャメルケースに従うよう修正しました

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -19,22 +19,22 @@ public class StreamMain {
         System.out.println(numList);
 
         //最大値の出力
-        Integer maxresult = numList.stream().max(Comparator.naturalOrder()).orElse(0);
-        System.out.println("リストの最大値は" + maxresult + "です。" );
+        Integer maxResult = numList.stream().max(Comparator.naturalOrder()).orElse(0);
+        System.out.println("リストの最大値は" + maxResult + "です。" );
 
         //最小値の出力
-        Integer minresult = numList.stream().min(Comparator.naturalOrder()).orElse(0);
-        System.out.println("リストの最小値は" + minresult + "です。");
+        Integer minResult = numList.stream().min(Comparator.naturalOrder()).orElse(0);
+        System.out.println("リストの最小値は" + minResult + "です。");
 
         //平均値の出力
-        double averageresult = numList.stream().mapToDouble(a -> a).average().orElse(0);
-        System.out.println("リストの平均値は" + averageresult + "です。");
+        double averageResult = numList.stream().mapToDouble(a -> a).average().orElse(0);
+        System.out.println("リストの平均値は" + averageResult + "です。");
 
-        List<Integer> evennamberresult = numList.stream().filter(i ->i%2==0).sorted().toList();
-        long countresult = numList.stream().filter(i ->i%2==0).count();
+        List<Integer> evennamberResult = numList.stream().filter(i ->i%2==0).sorted().toList();
+        long countResult = numList.stream().filter(i ->i%2==0).count();
         System.out.println("リストの偶数を表示します。");
-        System.out.println(evennamberresult);
-        System.out.println("偶数の値は" + countresult + "つです。");
+        System.out.println(evennamberResult);
+        System.out.println("偶数の値は" + countResult + "つです。");
 
 
     }
